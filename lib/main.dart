5053c386b792650c874175c780960b0c11067aab
ahmadjamil <ahmadjamil2.0000@gmail.com>
2025-08-18 10:18:21 -0700

    payment method implemented

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,16 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'Features/StripePaymentMethod/PaymentMethodScreen.dart';
 import 'Features/ToDoReminder/Themes/ThemeServices.dart';
 import 'Features/ToDoReminder/Themes/Themes.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey ='pk_test_51RxRqRRyIzZk0v3gN1wJyPUFDH8WAZtwd7HosbrMXXXZIogZusPVsza9sXTCqNmS8LjIxjyBhfrMpias5Bdy5K9400G2LwPQu5';
+  await Stripe.instance.applySettings();
+
   await DbHelper.initDb();
   await GetStorage.init();
   tz.initializeTimeZones();
@@ -35,7 +40,7 @@ class MyApp extends StatelessWidget
       theme: Themes.light,
       darkTheme: Themes.dark,
       themeMode: ThemeServices().themes, // set global theme mode
-      home: const FeaturesScreen(),
+      home: const PaymentMethodScreen(),
       // home: const MyHomePage(),
     );
   }
